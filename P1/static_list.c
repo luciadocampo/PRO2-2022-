2e360566aa37bd5a2b8b25c3717714a67efb45a2
Daniel Rodriguez Sanchez <dani.rodriguez.sanchez1@gmail.com>
2022-02-23 20:18:06 +0100

    P1 -> dynamic_list .h y .c (faltan por corregir funciones)

--- P1/static_list.c
+++ P1/static_list.c
@@ -1,10 +1,10 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Daniel Rodriguez Sanchez LOGIN 1: daniel.rodriguez.sanchez1
+ * AUTHOR 2: Lucia Docampo Rodriguez LOGIN 2: lucia.docampo
+ * GROUP: 4.2
+ * DATE: 23 / 02 / 22
  */
 
 #include "static_list.h"

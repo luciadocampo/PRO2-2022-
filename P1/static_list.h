1e2305d643717e2fe9cc8238ec288d7c7f00b45e
luciadocampo <lucia.docampo@udc.es>
2022-02-24 10:45:44 +0100

    mi static del año pasado

--- P1/static_list.h
+++ P1/static_list.h
@@ -10,8 +10,33 @@
 #ifndef STATIC_LIST_H
 #define STATIC_LIST_H
 
+#include "stdio.h"
+#include <stdbool.h>
 #include "types.h"
 
-/* Write your code here... */
+#define LNULL -1
+
+#define MAX 25
+
+
+typedef int tPosL;
+typedef struct  {
+    tItemL data[25];
+    tPosL LastPos;
+} tList;
+
+void createEmptyList (tList*);
+bool insertItem(tItemL, tPosL, tList*);
+bool copyList(tList, tList*);
+void updateItem(tItemL, tPosL, tList*);
+void deleteAtPosition(tPosL, tList*);
+//void deleteList(tList*);
+tPosL findItem(tNickname, tList);
+bool isEmptyList(tList);
+tItemL getItem(tPosL, tList);
+tPosL first(tList);
+tPosL last(tList);
+tPosL previous(tPosL, tList);
+tPosL next(tPosL, tList);
 
 #endif

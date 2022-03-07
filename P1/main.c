be869158f1add4966fcf24db56664bbf0c058647
luciadocampo <lucia.docampo@udc.es>
2022-03-07 09:25:12 +0100

    new terminado

--- P1/main.c
+++ P1/main.c
@@ -23,7 +23,29 @@
 
 //NEW: alta nuevo producto
 void New(tList *list, tProductId product, tUserId user, tProductCategory category, tProductPrice price){
-
+    tItemL item;
+    tPosL pos;
+    pos = findItem(product, *list);
+    if(pos == LNULL){
+        strcpy(item.productId, product);
+        strcpy(item.seller, user);
+        item.productCategory = category;
+        item.productPrice = price;
+        item.bidCounter = 0;
+        if(insertItem(item, LNULL, list)){
+            char* categoria = malloc(sizeof(char));
+            if(item.productCategory == 0){
+                strcpy(categoria, "book");
+            }else{
+                strcpy(categoria, "painting");
+            }
+            printf("* New: product %s seller %s category %s price %.2f\n", item.productId, item.seller, categoria, item.productPrice);
+        } else {
+            printf("+ Error: New not possible\n");
+        }
+    }else {
+        printf("+ Error: New not possible\n");
+    }
 }
 
 //DELETE: baja producto
@@ -41,24 +63,37 @@ void Stats(tList *list){
 
 }
 
+tProductCategory char_to_category(char* string){
+    tProductCategory category;
+    if(strcmp(string, "book") == 0){
+        return category = book;
+    }
+    if(strcmp(string, "painting") == 0){
+        return category = painting;
+    }
+    printf("Categoria inexistente");
+    return 0;
+}
+
 void processCommand(tList *list, char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4) {
 
     switch (command) {
         case 'N':
-            printf("********************");
+            printf("********************\n");
             printf("%s %c: product %s seller %s category %s price %s\n", commandNumber, command, param1, param2, param3, param4);
+            New(list, param1, param2, char_to_category(param3), (tProductPrice)strtod(param4, NULL));
             break;
         case 'S':
-            printf("********************");
+            printf("********************\n");
             printf("%s %c\n", commandNumber, command);
             break;
         case 'B':
-            printf("********************");
+            printf("********************\n");
             printf("%s %c: product %s bidder %s price %s\n", commandNumber, command, param1, param2, param4);
             break;
         case 'D':
-            printf("********************");
-            printf("%s %c: product %s", commandNumber, command, param1);
+            printf("********************\n");
+            printf("%s %c: product %s\n", commandNumber, command, param1);
             break;
         default:
             break;

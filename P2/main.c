f636d8dbd149ff68bd7296528096b9e9185b8558
luciadocampo <lucia.docampo@udc.es>
2022-04-18 13:29:15 +0200

    new y bid empezados

--- P2/main.c
+++ P2/main.c
@@ -11,15 +11,117 @@
 #include <stdlib.h>
 #include <string.h>
 #include "types.h"
+#include "product_list.h"
+#include "bid_stack.h"
 
 #define MAX_BUFFER 255
 
+void New(tList *list, tProductId product, tUserId user, tProductCategory category, tProductPrice price){
+    tPosL pos;
+    if((pos = findItem(product, *list)) != LNULL){ //si la posición no es nula devuelve error
+        printf("+ Error: New not possible\n");
+        return;
+    }
+    tItemL item;
+    char* categoria = malloc(sizeof(char)); //reserva de memoria para la categoría
+
+    //Metemos cada atributo en el item
+    item.productPrice = price;
+    item.productCategory = category;
+    strcpy(item.productId, product);
+    strcpy(item.seller, user);
+    item.bidCounter = 0;
+
+    createEmptyStack(&item.bidStack); //crea una pila vacía
+
+    if(insertItem(item, list)){ //Inserta un nuevo producto en la lista
+        if(item.productCategory == 0){
+            strcpy(categoria, "book");
+        }else{
+            strcpy(categoria, "painting");
+        }
+        printf("* New: product %s seller %s category %s price %.2f\n",
+               item.productId, item.seller, categoria, item.productPrice);
+    } else { //Error si el elemento no se inserta en la lista
+        printf("+ Error: New not possible\n");
+    }
+}
+
+void Delete(){
+
+}
+
+void Bid(tList *list, tProductId product, tUserId user, tProductPrice price){
+    tPosL pos;
+    tPosS posS;
+
+    if((pos = findItem(product, *list)) != LNULL){ //Comprueba si la posicion es nula o no
+        tItemL item;
+        tItemS itemS;
+        char* categoria = malloc(sizeof(char)); //reserva de memoria para la categoría
+        item = getItem(pos, *list); //metemos el item en una variable
+        if(strcmp(user, item.seller) != 0){ //comprobamos que el que puja no es vendedor
+            if(item.productPrice < price){ //Comprueba que el precio de la puja es mayor que el precio anterior
+
+                if(item.productCategory == 0){
+                    strcpy(categoria, "book");
+                }else{
+                    strcpy(categoria, "painting");
+                }
+
+                item.productPrice = price;
+                item.bidCounter++;  //incrementa la cuenta de pujas del usuario
+                updateItem(item, pos, list); //actualiza el contenido de esa posicion
+
+                printf("* Bid: product %s seller %s category %s price %.2f bids %d\n",
+                       item.productId, item.seller, categoria, item.productPrice, item.bidCounter);
+                return;
+            }
+        }
+    }
+    printf("+ Error: Bid not possible\n");
+}
+
+void Award(){
+
+}
+
+void Withdraw(){
+
+}
+
+void Remove(){
+
+}
+
+void Stats(){
+
+}
+
+//Funcion que convierte un char a la categoria
+tProductCategory char_to_category(char* string){
+    tProductCategory category;
+    if(strcmp(string, "book") == 0){
+        category = book;
+        return category;
+    }
+    if(strcmp(string, "painting") == 0){
+        category = painting;
+        return category;
+    }
+    printf("Categoria inexistente");
+    return 0;
+}
 
 
-void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4) {
+void processCommand(tList  *list, char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4) {
 
     switch (command) {
         case 'N':
+            printf("********************\n");
+            printf("%s %c: product %s seller %s category %s price %s\n", commandNumber, command, param1, param2, param3, param4);
+            New(list, param1, param2, char_to_category(param3), (tProductPrice)strtod(param4, NULL));
+
             break;
         case 'S':
             break;
@@ -44,6 +146,9 @@ void readTasks(char *filename) {
     const char delimiters[] = " \n\r";
     char buffer[MAX_BUFFER];
 
+    tList list;
+    createEmptyList(&list);
+
     f = fopen(filename, "r");
 
     if (f != NULL) {
@@ -56,7 +161,7 @@ void readTasks(char *filename) {
             param3 = strtok(NULL, delimiters);
             param4 = strtok(NULL, delimiters);
 
-            processCommand(commandNumber, command[0], param1, param2, param3, param4);
+            processCommand(&list, commandNumber, command[0], param1, param2, param3, param4);
         }
 
         fclose(f);
